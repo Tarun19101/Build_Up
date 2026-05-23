9b5be757072cfb7302f8dbd71b54d1a38f2622af
Tarun Balayan <tarunbalayan40@gmail.com>
2026-05-23 18:55:14 +0530

    Add UI layout and make different classes for navbar options to change widget.

--- app.py
+++ app.py
@@ -60,130 +60,163 @@ class RegisterPage(ctk.CTkFrame):
           Dashboard(self).pack(fill="both", expand=True)
           
 class navbar(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
-          super().__init__(master, **kwargs)
-          self._nav_ui()
-
-    def _nav_ui(self):
+                    
+    def __init__(self, master, app, **kwargs):
+        super().__init__(master, **kwargs)
+        self.app = app
         self.card = ctk.CTkFrame(self, fg_color="black")
         self.card.grid(row=0, column=0, sticky='ew')
         self.grid_columnconfigure(0, weight=1)
         
-        
-        self.dash_btn = ctk.CTkButton(self.card, text="Dashboard")
+        self.dash_btn = ctk.CTkButton(self.card, text="Dashboard", command=lambda: self.app.show_page(Dashboard))
         self.dash_btn.grid(row=0, column=0, padx=10, pady=10)
-        self.pro_btn = ctk.CTkButton(self.card, text="Profile")
+        self.pro_btn = ctk.CTkButton(self.card, text="Profile", command=lambda: self.app.show_page(Profile))
         self.pro_btn.grid(row=0, column=1, padx=10, pady=10)
-        self.task_btn = ctk.CTkButton(self.card, text="Tasks")
+        self.task_btn = ctk.CTkButton(self.card, text="Tasks", command=lambda: self.app.show_page(Tasks))
         self.task_btn.grid(row=0, column=2, padx=10, pady=10)
-        self.que_btn = ctk.CTkButton(self.card, text="Quests")
+        self.que_btn = ctk.CTkButton(self.card, text="Quests", command=lambda: self.app.show_page(Quests))
         self.que_btn.grid(row=0, column=3, padx=10, pady=10)
-        self.work_btn = ctk.CTkButton(self.card, text="Work")
+        self.work_btn = ctk.CTkButton(self.card, text="Work", command=lambda: self.app.show_page(Work))
         self.work_btn.grid(row=0, column=4, padx=10, pady=10)
-        self.bank_btn = ctk.CTkButton(self.card, text="Bank")
+        self.bank_btn = ctk.CTkButton(self.card, text="Bank", command=lambda: self.app.show_page(Bank))
         self.bank_btn.grid(row=0, column=5, padx=10, pady=10)
-        self.upd_btn = ctk.CTkButton(self.card, text="Updates")
+        self.upd_btn = ctk.CTkButton(self.card, text="Updates", command=lambda: self.app.show_page(Updates))
         self.upd_btn.grid(row=0, column=6, padx=10, pady=10)
-          
+        
 
          
-         
-         
-         
-class Dashboard(ctk.CTkFrame):
-     def __init__(self, master, **kwargs):
+        
+          
+
+class Base_layout(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
          super().__init__(master, **kwargs)
+         self.app = self.winfo_toplevel()
          
          self.grid_columnconfigure(0, weight = 1)
          self.grid_columnconfigure(1, weight = 1)
          self.grid_rowconfigure(1, weight = 1)
          self.grid_rowconfigure(2, weight = 1)
          
-         navbar(self).grid(row=0, column=0, sticky = 'nwe', columnspan=2, padx=5)
+         menu = navbar(self, self.app)
+         menu.grid(row=0, column=0, sticky = 'new', columnspan=2, padx=5)
          
-         self.card1 = ctk.CTkFrame(self, fg_color="white")
-         self.card1.grid(row=1, column=0, padx=10, pady=10, sticky = 'nwes')
-        
-         self.card2 = ctk.CTkFrame(self, fg_color="white")
-         self.card2.grid(row=1, column=1, padx=10, pady=10, sticky = 'nwes')
-        
-         self.card3 = ctk.CTkFrame(self, fg_color="white")
-         self.card3.grid(row=2, column=0, padx=10, pady=10, sticky = 'nwes')
-        
-         self.card4 = ctk.CTkFrame(self, fg_color="white")
-         self.card4.grid(row=2, column=1, padx=10, pady=10, sticky = 'nwes')
+         colors = ["red","blue","green","yellow"]
+         
+         for i,color in enumerate(colors, 1):
+            card = ctk.CTkFrame(self, fg_color=color)
+            card.grid(row=(1 if i<=2 else 2), column=(0 if i in [1,3] else 1), padx=10, pady=10, sticky = 'news')
+            inner = ctk.CTkFrame(card, fg_color="white")
+            inner.grid(row=0, column=0, padx=5, pady=5, sticky = 'news')
+            
+            card.grid_columnconfigure(0, weight = 1)
+            card.grid_rowconfigure(0, weight = 1)
+            
+            setattr(self, f"card{i}", card)
+            setattr(self, f"inner_card{i}", inner)
+            
+    def refresh_data(self):
+        pass
+             
+         
+         
+class Dashboard(Base_layout):
+    def __init__(self, master, **kwargs):
+         super().__init__(master, **kwargs)
+         ctk.CTkLabel(self.inner_card1, text="Dashboard Content", fg_color='black').pack(padx=10, pady=10)
+
+         
+    def refresh_data(self):
+        pass
          
          
 
-class Profile(ctk.CTkFrame):
-     def __init__(self, master, **kwargs):
+class Profile(Base_layout):
+    def __init__(self, master, **kwargs):
          super().__init__(master, **kwargs)
-         navbar(self).grid(row=0, column=0)
+         ctk.CTkLabel(self.inner_card1, text="Profile Content", fg_color='black').pack(padx=10, pady=10)
+
          
-         label = ctk.CTkLabel(self, text="Welcome to Dashboard!")
-         label.grid(row=1, column=0)
+    def refresh_data(self):
+        pass
          
-class Tasks(ctk.CTkFrame):
-     def __init__(self, master, **kwargs):
+        
+         
+class Tasks(Base_layout):
+    def __init__(self, master, **kwargs):
          super().__init__(master, **kwargs)
-         navbar(self).grid(row=0, column=0)
+         ctk.CTkLabel(self.inner_card1, text="Tasks Content", fg_color='black').pack(padx=10, pady=10)
+
          
-         label = ctk.CTkLabel(self, text="Welcome to Dashboard!")
-         label.grid(row=1, column=0)
+    def refresh_data(self):
+        pass
          
          
-class Quests(ctk.CTkFrame):
-     def __init__(self, master, **kwargs):
+class Quests(Base_layout):
+    def __init__(self, master, **kwargs):
          super().__init__(master, **kwargs)
-         navbar(self).grid(row=0, column=0)
+         ctk.CTkLabel(self.inner_card1, text="Quests Content", fg_color='black').pack(padx=10, pady=10)
+
          
-         label = ctk.CTkLabel(self, text="Welcome to Dashboard!")
-         label.grid(row=1, column=0)
+    def refresh_data(self):
+        pass
          
-class Work(ctk.CTkFrame):
-     def __init__(self, master, **kwargs):
+class Work(Base_layout):
+    def __init__(self, master, **kwargs):
          super().__init__(master, **kwargs)
-         navbar(self).grid(row=0, column=0)
+         ctk.CTkLabel(self.inner_card1, text="Work Content", fg_color='black').pack(padx=10, pady=10)
+
          
-         label = ctk.CTkLabel(self, text="Welcome to Dashboard!")
-         label.grid(row=1, column=0)
+    def refresh_data(self):
+        pass
          
-class Bank(ctk.CTkFrame):
-     def __init__(self, master, **kwargs):
+class Bank(Base_layout):
+    def __init__(self, master, **kwargs):
          super().__init__(master, **kwargs)
-         navbar(self).grid(row=0, column=0)
+         ctk.CTkLabel(self.inner_card1, text="Bank Content", fg_color='black').pack(padx=10, pady=10)
+
          
-         label = ctk.CTkLabel(self, text="Welcome to Dashboard!")
-         label.grid(row=1, column=0)
+    def refresh_data(self):
+        pass
          
-class Updates(ctk.CTkFrame):
-     def __init__(self, master, **kwargs):
+class Updates(Base_layout):
+    def __init__(self, master, **kwargs):
          super().__init__(master, **kwargs)
-         navbar(self).grid(row=0, column=0)
+         ctk.CTkLabel(self.inner_card1, text="Updates Content", fg_color='black').pack(padx=10, pady=10)
+
          
-         label = ctk.CTkLabel(self, text="Welcome to Dashboard!")
-         label.grid(row=1, column=0)
+    def refresh_data(self):
+        pass
+         
+         
 class App(ctk.CTk):
-      def __init__(self):
-          super().__init__()
-          self.title("Build Up")
-          self.geometry("1920x1080")
+    def __init__(self):
+        super().__init__()
+        self.title("Build Up")
+        self.geometry("1920x1080")
 
-          database.create_tables()
-          see_user = database.check_user()
-
-          self._page(see_user)
-
-      def _page(self, see_user):
-          if see_user:
-              self.show_page(Dashboard)
-          else:
-              self.show_page(RegisterPage)
-
-      def show_page(self, page_class):
-          for widget in self.winfo_children():
-              widget.destroy()
-          page_class(self).pack(fill="both", expand=True)
+        database.create_tables()
+        see_user = database.check_user()
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        
+        self.pages = {}
+        for PageClass in [Dashboard, Profile, Tasks, Quests, Work, Bank, Updates]:
+            page = PageClass(self)
+            page.grid(row=0, column=0, sticky="nsew")
+            self.pages[PageClass] = page
+            
+        self.show_page(Dashboard if see_user else RegisterPage)
+    
+    def show_page(self, page_class):
+        if page_class == RegisterPage:
+            for widget in self.winfo_childern():
+                widget.destroy()
+            RegisterPage(self).pack(fill="both", expand=True)
+        else:
+            self.pages[page_class].tkraise()
+            if hasattr(self.pages[page_class], 'refresh_data'):
+                self.pages[page_class].refresh_data()
 
 if __name__ == "__main__":
       app = App()
